1b8f6feb20c5ef4abf58c8d27f56fa18122301da
Francesco Frassinelli <fraph24@gmail.com>
2017-08-17 17:58:45 +0200

    Initial import

---
+++ assets/overpass.js
@@ -0,0 +1,99 @@
+const provider = new window.GeoSearch.OpenStreetMapProvider();
+const searchControl = new window.GeoSearch.GeoSearchControl({
+  provider: provider,
+  style: 'bar',
+  autoClose: true
+}); 
+
+let map = L.map('map');
+map.locate({watch: true});
+
+map.addControl(searchControl);
+map.on('geosearch_showlocation', (result) => {
+ console.log(result); // location + marker
+});
+
+let custom_attribution = `${document.title} (ideato da <a href="https://frafra.eu">Frafra</a>)`;
+let OpenStreetMapLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: `${custom_attribution} | &copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>`,
+  maxZoom: 18,
+  minZoom: 15
+});
+
+let baseLayers = {
+  "OpenStreetMap":OpenStreetMapLayer,
+};
+OpenStreetMapLayer.addTo(map);
+
+let overlays = {};
+for (let feature in features) {
+  let name = `<span style="color:${features[feature].color}">&#x25cf;</span> ${features[feature].name}`;
+  features[feature].name = name;
+  overlays[name] = L.layerGroup();
+  overlays[name].addTo(map);
+}
+L.control.layers(baseLayers, overlays, {collapsed:false}).addTo(map);
+
+function getFeatures() {
+  if (map.getZoom() < 15) return;
+  let bounds = map.getBounds();
+  let bbox = bounds.getSouth()+','+bounds.getWest()+','+bounds.getNorth()+','+bounds.getEast();  
+  let request = "";
+  for (let feature in features) {
+    request += `node[${feature}](${bbox});`;
+    request += `way[${feature}](${bbox});`;
+  }
+  let url = `https://overpass-api.de/api/interpreter?data=[out:json][timeout:25];(${request});out;`;
+  fetch(url).then(response => {
+    return response.json();
+  }).then(results => {
+    for (let feature in features) {
+      overlays[features[feature].name].clearLayers();
+    }
+    results.elements.forEach(e => {
+      let featureFound;
+      for (let feature in features) {
+        let keyValue = feature.split('=', 2);
+        let key = keyValue[0];
+        let value = keyValue[1].split('"')[1];
+        if (key in e.tags) {
+          if (value == e.tags[key]) {
+            featureFound = feature;
+          }
+        }
+      }
+      let color = features[featureFound].color;
+      let marker = L.circleMarker([e.lat, e.lon], {color: color});
+      let content = '';
+      for (let tag in e.tags) {
+        content += `<b>${tag}</b>: ${e.tags[tag]}<br/>`;
+      };
+      marker.bindPopup(content)
+      marker.addTo(overlays[features[featureFound].name]);
+    });
+  }).catch(error => {
+    waitAndReload();
+  });
+}
+
+let timer;
+function waitAndReload() {
+  window.clearTimeout(timer);
+  timer = setTimeout(getFeatures, 1000);
+}
+
+let position;
+function onLocationFound(e) {
+  if (position) {
+    position.remove();
+  } else {
+    map.setView(e.latlng, 16);
+  }
+  position = L.marker(e.latlng)
+  position.bindPopup('Tu sei qui :)')
+  position.addTo(map);
+}
+
+map.on('locationfound', onLocationFound);
+map.on('zoomend', waitAndReload);
+map.on('moveend', waitAndReload);
---
+++ configuration.js
@@ -0,0 +1,24 @@
+document.title = `${document.title} [Ciclisti]`;
+
+let features = {
+  'amenity="pharmacy"':{
+    name:`Farmacie`,
+    color:"#a700c8"
+  },
+  'amenity="drinking_water"':{
+    name:`Fontanelle`,
+    color:"#3388ff"
+  },
+  'shop="bicycle"':{
+    name:`Vendita biciclette`,
+    color:"#ff5b02"
+  },
+  'amenity="bicycle_parking"':{
+    name:`Rastrelliere`,
+    color:"#c92626"
+  },
+  'amenity="toilets"':{
+    name:`Servizi igenici`,
+    color:"#00d180"
+  }
+}
